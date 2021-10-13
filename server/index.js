9c925500e512f1db04c3a1529b7b84a5f92353bf
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-10-13 19:12:00 +0530

    added nodemon, mongoose and cors to server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+dotenv.config();
 
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("Connected to DB!")
+);
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server Running on PORT: ${PORT}`);
-})
+  console.log(`Server Running on PORT: ${PORT}`);
+});
